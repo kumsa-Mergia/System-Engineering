6941f8fa6c720d38759705b1fb998dca21396edf
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:06:07 +0300

    Create mv-command.sh

---
+++ Linux commands/mv-command.sh
@@ -0,0 +1,15 @@
+# The main usage of the mv command is to move a file or folder to another location. Here’s the syntax:
+
+mv file_or_directory [target_directory]
+
+# For example, we will move file1.txt from another location to the /new/file/directory path using this command:
+
+mv /original/path/file1.txt the/target/path
+
+# You can also use the mv command to rename files in your Linux system. Here’s an example:
+
+mv old_name.txt new_name.txt
+
+# If you specify the full path, you can simultaneously rename files and move them to a new location like this example:
+
+mv old/location/of/old_name.txt new/path/for/new_name.txt
