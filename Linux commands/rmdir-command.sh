c2efdeb2946bb2d152160ec24df24e998adda3f4
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:02:44 +0300

    Create rmdir-command.sh

---
+++ Linux commands/rmdir-command.sh
@@ -0,0 +1,5 @@
+# Run rmdir to delete empty directories in your Linux system. The command syntax looks like this:
+
+rmdir [options] directory_name
+
+# The rmdir command won’t work if the directory contains subfolders. To force the deletion, add the –p option. Note that you must own the item you want to remove or use sudo instead.
