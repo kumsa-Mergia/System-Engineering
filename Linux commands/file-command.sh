cf6b0d993481675316c549e67c7fdf06b5e1b0d7
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:08:06 +0300

    Create file-command.sh

---
+++ Linux commands/file-command.sh
@@ -0,0 +1,8 @@
+# Run the touch command to create a new empty file in a specific directory. The syntax is as follows:
+
+touch [options] [path_and_file_name]
+
+
+# If you omit the path, the touch command will create a new file in your current working directory. Here’s an example:
+
+touch file.txt
