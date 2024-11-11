5cafd24e9d6e59d3f2b41e98085771d6bdd4c25e
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:00:07 +0300

    Create cd-command.sh

---
+++ Linux commands/cd-command.sh
@@ -0,0 +1,9 @@
+# Use cd to navigate between directories in your Linux VPS. It doesn’t have any option, and the syntax is simple:
+
+cd [path_or_directory]
+
+# Depending on your location, you might only need to specify the parent directory. For example, omit path from path/to/directory if you are already inside one. The cd command has several shortcuts:
+
+    # cd – returns to the current user’s home directory.
+    # cd .. – moves a directory up.
+    # cd – – goes back to the previous directory.
