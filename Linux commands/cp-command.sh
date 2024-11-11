fdaec1ee310eb4ecabb79075f9310c115ab88c3b
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:05:06 +0300

    Create cp-command.sh

---
+++ Linux commands/cp-command.sh
@@ -0,0 +1,11 @@
+# Use the cp command to copy files from your current directory to another folder. The syntax looks like this:
+
+cp file1 file2 [target_path]
+
+# You can also use cp to duplicate the content of one file to another using this syntax. If the target is in another location, specify the full path like so:
+
+cp source_file /path/to/target_file
+
+# Additionally, cp lets you duplicate a directory and its content to another folder using the -R option:
+
+cp -R /path/to/folder /target/path/to/folder_copy
