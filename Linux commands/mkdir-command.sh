73bdece839ce4344c5feb2e8782a3fa07bfd6f68
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:01:47 +0300

    Create mkdir-command.sh

---
+++ Linux commands/mkdir-command.sh
@@ -0,0 +1,11 @@
+# The mkdir command lets you create one or multiple directories. The syntax looks like this:
+
+mkdir [options] directory_name1 directory_name2
+
+# To create a folder in another location, specify the full path. Otherwise, this command will make the new item in your current working directory.
+
+# For example, enter the following to create new_folder in /path/to/target_folder:
+
+mkdir path/to/target_folder/new_folder
+
+# By default, mkdir allows the current user to read, write, and execute files in the new folder. You can set custom privileges during the creation by adding the -m option. To learn more about permission management, read the chmod section below.
