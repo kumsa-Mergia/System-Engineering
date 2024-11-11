bb666681275b9beec4c9a30bc7fd75190b79267a
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 13:03:54 +0300

    Create rm-command.sh

---
+++ Linux commands/rm-command.sh
@@ -0,0 +1,7 @@
+# The rm command deletes files from a directory. You must have the write permission for the folder or use sudo. Here’s the syntax:
+
+rm [options] file1 file2
+
+# You can add the -r option to remove a folder and its contents, including subdirectories. Use the -i flag to display a confirmation message before the removal or -f to deactivate it completely.
+
+# NOTE: Warning! Avoid using -r and -f unless necessary. Instead, add -i option to prevent accidental deletion. 
