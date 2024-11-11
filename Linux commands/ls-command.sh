65b73ef41c4e9be43410fd27fb4ce902a3f662c6
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-11 12:56:09 +0300

    Create ls-command.sh

---
+++ Linux commands/ls-command.sh
@@ -0,0 +1,5 @@
+// The ls command lists the content of a folder, including files and directories. Here’s the syntax:
+
+ls [options] [directory_or_path]
+
+// If you omit the path, the ls command will check the content of your current directory. To list items inside subfolders, add the -R option. Meanwhile, use -a to show hidden content.
