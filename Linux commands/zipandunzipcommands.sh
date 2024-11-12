5d460c7ab279df1cf36e687b73d48022ddedd788
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-12 14:09:17 +0300

    Create zipandunzipcommands

---
+++ Linux commands/zipandunzipcommands.sh
@@ -0,0 +1,7 @@
+# The zip command compresses one or multiple files into a ZIP archive, reducing their size. Here’s the syntax:
+
+zip [options] zip_file_name file1 file2
+
+# To extract a compressed file into your current working directory, use the unzip command like so:
+
+unzip [options] zip_file_name
