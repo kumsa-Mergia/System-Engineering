b44c0fcced1c4908737d8ce263deea4c49776535
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-12 14:15:51 +0300

    Create head-command.sh

---
+++ Linux commands/head-command.sh
@@ -0,0 +1,9 @@
+# Use the head command to print the first few entries of a file. The basic syntax is as follows:
+
+head [options] file_name
+
+# You can also print the first few lines of another command’s output by piping it like so:
+
+command | head [options]
+
+# By default, head will show the first ten lines. However, you can change this setting using the -n option followed by your desired number.
