b1104b8f5494211966fa28155ff9d58bd8994f2e
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 08:03:26 +0300

    su and whoami commands

---
+++ Linux commands/su-and-whoami-commands.sh
@@ -0,0 +1,11 @@
+# su and whoami commands
+
+# The su command lets you switch to another user in the Terminal session. The syntax looks as follows:
+
+su [options] [username]
+
+# If you don’t specify any option or username, this command will switch you to the root user. In this case, you must enter the password before changing the account.
+
+# You can check the currently logged-in user from the Linux command-line shell. Alternatively, use the whoami command:
+
+whoami
