97ed60da01c2b17c6d7f3dc2e51d40d1e8356ee1
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 08:00:12 +0300

    tee command

---
+++ Linux commands/tee-command.sh
@@ -0,0 +1,5 @@
+# tee command
+
+# The tee command outputs another command’s results to both the Terminal and a file. It’s helpful if you want to use the data for further processing or backups. Here’s the syntax:
+
+command | tee [options] file_name
