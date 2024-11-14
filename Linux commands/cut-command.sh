fa6454fe62fd1cfd4c3aa13c3259d54eac55a753
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 07:58:07 +0300

    cut command

---
+++ Linux commands/cut-command.sh
@@ -0,0 +1,12 @@
+# cut command
+
+# The cut command selects specific sections from a file and prints them as a Terminal output. The syntax looks like this:
+
+cut options file
+
+# Unlike other Linux utilities, the cut command’s options are mandatory for file sectioning. Here are some of the flags:
+
+  #  -f – selects a specific row field.
+  #     -b – cuts the line by a specified byte size.
+  #     -c – sections the line using a specified character.
+  #     -d – separates lines based on delimiters.
