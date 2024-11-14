29a69f14123d08d8c26e8c9d318e838c221978e2
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 08:02:30 +0300

    sudo command

---
+++ Linux commands/sudo-command.sh
@@ -0,0 +1,9 @@
+# sudo command
+
+# superuser do or sudo enables non-root users who are part of the sudo group to execute administrative commands. Simply add it at the beginning of another utility like so:
+
+sudo [options] your_command
+
+# For example, enter the following to open a file using nano as an administrator:
+
+sudo nano file.txt
