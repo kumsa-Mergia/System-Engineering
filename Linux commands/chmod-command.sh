af20c212ac0beffe41f05dddaaf0f32cb9aa5440
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 08:04:17 +0300

    chmod command

---
+++ Linux commands/chmod-command.sh
@@ -0,0 +1,9 @@
+# chmod command
+
+# Chmod lets you change the permissions of files or directories. The basic syntax looks as follows:
+
+chmod [options] [permission] [file_or_directory]
+
+# In Linux, there are three folder and file permissions – read (r), write (w), and execute (x). You can assign them to three parties – the owner, a group, or other accounts belonging to neither category. Consider this example:
+
+chmod -rwx---r-– file1.txt
