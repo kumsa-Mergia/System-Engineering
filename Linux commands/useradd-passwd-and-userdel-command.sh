61bdfdeb62850c78a347980b8f9f830961f9d08a
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 08:06:27 +0300

    useradd, passwd, and userdel command

---
+++ Linux commands/useradd-passwd-and-userdel-command.sh
@@ -0,0 +1,13 @@
+# useradd, passwd, and userdel command
+
+# Use the useradd command to create a new account in your Linux system. The syntax is as follows:
+
+useradd [options] new_username
+
+# By default, the useradd command doesn’t prompt you to give the new user a password. You can add or change it manually later with the passwd command:
+
+passwd new_username
+
+# To remove a user, use the userdel command followed by the account name like the syntax in the example:
+
+userdel new_username
